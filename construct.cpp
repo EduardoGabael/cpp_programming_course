161a8122440187c46e042b79579fcd65986c9c24
Eduardo Gabael <eduardogabaelbr@gmail.com>
2022-04-21 14:09:06 -0500

    First class added

---
+++ construct.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+
+
+class User
+{
+   std::string status = "Tired";
+
+   public :
+        std::string first_name;
+        std::string last_name;
+        std::string get_status()
+        {
+            return status;
+        }
+
+};
+
+int main()
+{
+    User me;
+    me.first_name = "Eduardo";
+    me.last_name = "Beltran";
+
+    std::cout<<"Status: "<<me.get_status()<<std::endl;
+
+    return 0;
+}
