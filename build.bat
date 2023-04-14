9e6b12d8e0247e2c86d4ab1b3285866355fcc119
Eduardo Gabael <82793322+EduardoGabael@users.noreply.github.com>
2023-04-14 17:57:28 -0600

    build file modified to run files

--- build.bat
+++ build.bat
@@ -1,7 +1,21 @@
 @echo off 
-echo "Hola pinches putos"
+@echo off 
 
-mkdir build
-cd build
-cmake .. -Wno-dev -G "Unix Makefiles"
-make
+if %1% NEQ ""(
+    if %1% == test  goto test 
+    if %1% == run   goto run 
+) else(
+    goto error
+)
+
+:test
+        mkdir build
+        cd build
+        cmake .. -Wno-dev -G "Unix Makefiles"
+        make
+        goto exit
+:run 
+        cd build 
+        ./executable
+:exit
+        echo "Done!"
