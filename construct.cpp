f61efcbe711e0b4b49926a2a2298a5a2263bae62
Eduardo Gabael <82793322+EduardoGabael@users.noreply.github.com>
2023-04-19 11:34:21 -0600

    usleep adeed

--- construct.cpp
+++ construct.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<unistd.h>
 
 
 class User
@@ -23,7 +24,7 @@ int main()
     User me;
     me.first_name = "Eduardo";
     me.last_name = "Beltran";
-
+    usleep(200);
     std::cout<<"Status: "<<me.get_status()<<std::endl;
     return 0;
 }
